91e4329d7133f1cc4bad6c8a00f5bc2b4c576a53
Ross Queripel <rossqueripel@outlook.com>
2017-11-04 17:29:35 +0000

    wip

--- resources/views/layouts/components/nav.blade.php
+++ resources/views/layouts/components/nav.blade.php
@@ -1,10 +1,10 @@
-<nav role="main-navigation" class="grey-gradient w-full h-12">
+<nav role="main-navigation" class="grey-gradient w-full h-12 text-sm">
     <div class="container px-8 mx-auto">
         <div class="flex items-center flex-row">
             <div class="nav-item">
                 <a href="https://flyuk-design.dev">Home</a>
             </div>
-            <div class="nav-item">
+            <div class="nav-item-small">
                 <a href="https://flyuk-design.dev">About Us</a>
                 <ul class="dropdown-menu">
                     <li><a href="">About Fly UK</a></li>
@@ -13,8 +13,26 @@
                     <li><a href="">Staff Team</a></li>
                 </ul>
             </div>
-            <div class="nav-item">
+            <div class="nav-item-large">
                 <a href="https://flyuk-design.dev">Operations</a>
+                <div class="dropdown-menu">
+                    <div class="row">
+                        <div class="flex flex-wrap md:w-3/4 px-4">
+                            <div class="flex-none w-24 h-24 m-4 bg-grey-light border border-grey shadow"></div>
+                            <div class="flex-none w-24 h-24 m-4 bg-grey-light border border-grey shadow"></div>
+                            <div class="flex-none w-24 h-24 m-4 bg-grey-light border border-grey shadow"></div>
+                            <div class="flex-none w-24 h-24 m-4 bg-grey-light border border-grey shadow"></div>
+                            <div class="flex-none w-24 h-24 m-4 bg-grey-light border border-grey shadow"></div>
+                            <div class="flex-none w-24 h-24 m-4 bg-grey-light border border-grey shadow"></div>
+                            <div class="flex-none w-24 h-24 m-4 bg-grey-light border border-grey shadow"></div>
+                            <div class="flex-none w-24 h-24 m-4 bg-grey-light border border-grey shadow"></div>
+
+                        </div>
+                        <div class="flex md:w-1/4">
+                            Side links
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="nav-item">
                 <a href="https://flyuk-design.dev">Crew Centre</a>
